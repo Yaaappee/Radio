d154d0b1bd3d2d2df353ca84b155be757e1a77ef
Yaaappee <yaaappee@gmail.com>
2016-03-16 11:30:09 +0200

    Add sceleton of solution

---
+++ Radio/Radio/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace Radio
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        public class Name
+        {
+            
+        }
+    }
+}
